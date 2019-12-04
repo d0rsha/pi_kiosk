47740f48bdbe7ae2055ffcae6d50621054ba12d8
Project Ngulia pi <anders.vrethem@hiq.se>
2019-12-04 15:30:33 +0000

    Upgrade ping.sh; Interactive script; w8 for WiFi/ethernet

--- scripts/ping.sh
+++ scripts/ping.sh
@@ -3,29 +3,30 @@ xset s noblank
 xset s off
 xset -dpms
 
-#############
-## Spinner ##
-sping[0]="- Waiting for network   "
-sping[1]="\\ Waiting for network.  "
-sping[2]="| Waiting for network.. "
-sping[3]="/ Waiting for network..."
-#############
+echo "-------------[ Chromium kiosk ]------------" 
+echo "Waiting for WiFi connection..."
+while true; do
+    if ifconfig wlan0 | grep inet > /dev/null; then
+	break
+    fi
+    if ifconfig enxb827eb64eb3f | grep inet > /dev/null; then
+	break
+    fi
+true; done
+echo "Interface wlan0 is up!"
+echo "Network is up!"
+echo "Checking connection to server..."
+echo "Please hold on"
 
 while true
 do
     echo -ne "\r"
     echo "wget -q --spider https://dashboard.projectngulia.org"
-    if timeout 3 wget -q --spider https://dashboard.projectngulia.org > /dev/null; then
+    if wget --quiet --timeout=4 --tries=2 --spider https://dashboard.projectngulia.org > /dev/null; then
         break
-    else
-        for x in {1..3}; do
-        for i in "${sping[@]}"
-        do
-            echo -ne "\r$i"
-            sleep 0.25
-        done; done
     fi
 done
 
 echo "<light-ping.sh> Established connection to https://dashboard.projectngulia.org"
 echo "<light-ping.sh> Network online"
+echo "-------------[ Chromium kiosk ]------------" 
