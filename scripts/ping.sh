7905dd20a9d8cdb4ca98e4093f079bd5832568fc
Project Ngulia pi <anders.vrethem@hiq.se>
2019-12-04 15:40:31 +0000

    Make ping.sh interactive

--- scripts/ping.sh
+++ scripts/ping.sh
@@ -4,16 +4,17 @@ xset s off
 xset -dpms
 
 echo "-------------[ Chromium kiosk ]------------" 
-echo "Waiting for WiFi connection..."
+echo "Waiting for WiFi/ethernet connection..."
 while true; do
     if ifconfig wlan0 | grep inet > /dev/null; then
+	echo "Interface wlan0 is up!"
 	break
     fi
     if ifconfig enxb827eb64eb3f | grep inet > /dev/null; then
+	echo "Interface enxb827eb64eb3f is up!"
 	break
     fi
 true; done
-echo "Interface wlan0 is up!"
 echo "Network is up!"
 echo "Checking connection to server..."
 echo "Please hold on"
@@ -29,4 +30,6 @@ done
 
 echo "<light-ping.sh> Established connection to https://dashboard.projectngulia.org"
 echo "<light-ping.sh> Network online"
+echo "Opening browser..."
+echo "Please hold on"
 echo "-------------[ Chromium kiosk ]------------" 
